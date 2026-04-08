78ad7f73755720a22d2f04c2dac94f10be9d891e
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-04-08 09:00:43 -0300

    feat: update exercise 13

--- src/Gabriel13.java
+++ src/Gabriel13.java
@@ -7,22 +7,44 @@ public class Gabriel13 {
         // realização 2 copas mundiais. Sabendo dessas informações, faça um algoritmo em JAVA que dado o ano
         // atual calcule o número de copas já realizadas e exiba o resultado ao usuário.
 
-        int firstYearCoup, currentYearCoup, intervalInYears, countCoups;
+        int firstYearCoup, currentYear, intervalInYears, countCoups, initGapWWII, finishGapWWII;
 
         System.out.println(":: Cálculo da quantidade de Copas do Mundo FIFA realizadas ::");
         Scanner keyboard = new Scanner(System.in);
 
+        countCoups = 0;
         intervalInYears = 4;
         firstYearCoup = 1930;
+        initGapWWII = 1942;
+        finishGapWWII = 1946;
 
         System.out.println("Qual é o ano atual? ");
-        currentYearCoup = keyboard.nextInt();
+        currentYear = keyboard.nextInt();
 
-        // Subtrai do ano atual o primeiro ano que teve copa, divide pelo intervalo em anos e subtraí 2 (quantidade de anos que não tiveram copas)
-        // Para considerar 1930, soma-se + 1
-        countCoups = ((currentYearCoup - firstYearCoup) / intervalInYears) - 2 + 1;
+        // TODO - fazer validações
+        // IF condicao e ELSE condicao
+
+        if (currentYear < firstYearCoup) {
+            System.out.println("O ano informado é anterior a primeira Copa.");
+        }
+
+        // 1947 => TRUE && FALSE
+        // 1930 1934 1938 => 3 copas
+
+        // TODO - verificar se essa lógica está correta (parece que não)
+
+        else if (currentYear >= initGapWWII && currentYear <= finishGapWWII) {
+            // Não subtrai, pois os estamos no período em que não houve copa ou anterior a ele
+            countCoups = ((currentYear - firstYearCoup) / intervalInYears) + 1;
+        }
+
+        else {
+            // Subtrai do ano atual o primeiro ano que teve copa, divide pelo intervalo em anos e subtraí 2 (quantidade de anos que não tiveram copas)
+            // Para considerar 1930, soma-se + 1
+            countCoups = ((currentYear - firstYearCoup) / intervalInYears) - 2 + 1;
+        }
 
         System.out.println();
-        System.out.printf("Entre %d e %d tiveram %d copas.", firstYearCoup, currentYearCoup, countCoups);
+        System.out.printf("Entre %d e %d tiveram %d copas.", firstYearCoup, currentYear, countCoups);
     }
 }
