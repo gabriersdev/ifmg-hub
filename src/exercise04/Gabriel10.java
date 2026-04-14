c97edcd19584f14b83c51fbfbd53593ef8d36788
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-04-14 19:37:56 -0300

    feat: update exercise's

--- src/exercise04/Gabriel10.java
+++ src/exercise04/Gabriel10.java
@@ -1,5 +1,7 @@
 package exercise04;
 
+import java.util.Scanner;
+
 public class Gabriel10 {
     public static void main(String[] args) {
         // Faça um programa que recebe de entrada o código de um produto e a quantidade comprada do mesmo.
@@ -9,5 +11,37 @@ public class Gabriel10 {
         // c) O valor do desconto, segundo Tabela II, aplicado sobre o valor total da nota
         // d) Preço final ao consumidor após desconto
         // e) Não se esqueça de verificar a existência do produto
+
+        int code, quantity;
+        double priceItemsRange1, priceItemsRange2, priceItemsRange3, priceItemsRange4;
+        double percentageDiscountMax250, percentageDiscountMax500, percentageDiscountMore500, totalNote = 0, total = 0;
+        Scanner keyboard = new Scanner(System.in);
+
+        priceItemsRange1 = 10;
+        priceItemsRange2 = 20;
+        priceItemsRange3 = 30;
+        priceItemsRange4 = 40;
+
+        percentageDiscountMax250 = 0.05;
+        percentageDiscountMax500 = 0.1;
+        percentageDiscountMore500 = 0.15;
+
+        System.out.println("Informe o código do produto: ");
+        code = keyboard.nextInt();
+
+        System.out.println("Informe a quantidade de itens: ");
+        quantity = keyboard.nextInt();
+
+        if (code >= 1 && code <= 10) totalNote = quantity * priceItemsRange1;
+        else if (code <= 20) totalNote = quantity * priceItemsRange2;
+        else if (code <= 30) totalNote = quantity * priceItemsRange3;
+        else if (code <= 40) totalNote = quantity * priceItemsRange4;
+
+        if (totalNote <= 250) totalNote = totalNote * (1 - percentageDiscountMax250);
+        else if (totalNote <= 500) totalNote = totalNote * (1 - percentageDiscountMax500);
+        else totalNote = totalNote * (1 - percentageDiscountMore500);
+
+        System.out.println();
+        System.out.printf("Total à pagar: R$ %.2f", totalNote);
     }
 }
--- src/exercise04/Gabriel11.java
+++ src/exercise04/Gabriel11.java
@@ -1,5 +1,7 @@
 package exercise04;
 
+import java.util.Scanner;
+
 public class Gabriel11 {
     public static void main(String[] args) {
         // Faça um algoritmo que receba o salário base e o tempo de serviço de um funcionário. Calcule e mostre:
@@ -7,5 +9,51 @@ public class Gabriel11 {
         // A gratificação em porcentagem, conforme tabela:
         // O salário líquido, ou seja, salário base menos imposto mais gratificação
         // A categoria que está na tabela a seguir:
+
+        double baseSalary, gratificationPercentage, salaryLiquid, taxPercentage, gratification, tax;
+        int serviceTime;
+        String category; // A, B ou C
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Qual o seu salário base em R$? ");
+        baseSalary = keyboard.nextDouble();
+
+        System.out.println("Qual o seu tempo de serviço EM ANOS? ");
+        serviceTime = keyboard.nextInt();
+
+        // Verificando imposto
+        if (baseSalary < 200) taxPercentage = 0;
+        else if (baseSalary <= 450) taxPercentage = 0.03;
+        else if (baseSalary < 700) taxPercentage = 0.08;
+        else taxPercentage = 0.12;
+
+        // Cálculo da gratificação
+        if (baseSalary <= 500) {
+            if (serviceTime <= 3) gratificationPercentage = 0.23;
+            else if (serviceTime <= 6) gratificationPercentage = 0.35;
+            else gratificationPercentage = 0.33;
+        }
+
+        else {
+            if (serviceTime <= 3) gratificationPercentage = 0.2;
+            else gratificationPercentage = 0.3;
+        }
+
+        gratification = (baseSalary * gratificationPercentage);
+        tax = (baseSalary * taxPercentage);
+        salaryLiquid = baseSalary - tax + gratification;
+
+        if (salaryLiquid <= 350) category = "A";
+        else if (salaryLiquid <= 600) category = "B";
+        else category = "C";
+
+        System.out.println();
+        System.out.printf("O valor do imposto é R$ %.2f", tax);
+        System.out.println();
+        System.out.printf("O valor da gratificação é %.2f", gratification);
+        System.out.println();
+        System.out.printf("O seu salário líquido é de R$ %.2f", salaryLiquid);
+        System.out.println();
+        System.out.printf("A categoria em que você se enquadra é a %s", category);
     }
 }
