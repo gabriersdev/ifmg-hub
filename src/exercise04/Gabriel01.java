14eb5f2db7cc40d5100d1cb95f88f367637965dc
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-04-15 09:07:04 -0300

    feat: update exercises

--- src/exercise04/Gabriel01.java
+++ src/exercise04/Gabriel01.java
@@ -1,9 +1,42 @@
 package exercise04;
 
+import java.util.Scanner;
+
 public class Gabriel01 {
     public static void main(String[] args) {
         // Refaça o exercício referente à equação do segundo grau do roteiro anterior considerando todas as
         // condições possíveis, conforme fluxograma abaixo.
         // ax² + bx + c = 0
+
+        double varA = 0, varB = 0, varC = 0, varD = 0, varR = 0, varR1 = 0, varR2 = 0;
+        Scanner keyboard = new Scanner(System.in);
+
+        if (varA == 0) {
+            System.out.println("Não é uma equação do 2º grau.");
+
+            if (varB == 0) System.out.println("Não é uma equação do 1º grau.");
+
+            //
+            else {
+                varR = -varC / varB;
+                System.out.println(varR);
+            }
+        }
+
+        else {
+            varD = varB * varB - (4 * varA * varC);
+            if (varD < 0) System.out.println("Não há raízes reais.");
+
+            //
+            else {
+                varR1 = (-varB - Math.sqrt(varD)) / (2 * varA);
+                varR2 = (-varB + Math.sqrt(varD)) / (2 * varA);
+            }
+
+            System.out.println(varR1);
+            System.out.println(varR2);
+        }
+
+        keyboard.close();
     }
 }
