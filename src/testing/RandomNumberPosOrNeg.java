06ceef799b8715fab5829a6644537f8da8d3b0bd
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-06-02 09:56:52 -0300

    feat: base scripts

---
+++ src/testing/RandomNumberPosOrNeg.java
@@ -0,0 +1,22 @@
+package testing;
+
+import java.util.Random;
+
+public class RandomNumberPosOrNeg {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int a;
+
+        // Aqui sorteia um número, ou é 1 ou é 0
+        if (random.nextInt(2) == 1) {
+            // Vai sortear positivo
+            a = random.nextInt(100 + 1);
+            System.out.println(a);
+        } else {
+            // Vai sortear negativo
+            a = random.nextInt(100 + 1) * -1;
+            System.out.println(a);
+        }
+    }
+}
---
+++ src/testing/RememberFunctions.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class RememberFunctions {
+    public static void main(String[] args) {
+        // Chama a função estática criada abaixo
+        RememberFunctions.func();
+    }
+
+    public static void func() {
+        System.out.println("Função chamada!");
+    }
+}
