1d23dd1a21a012650a1aeaa208cad68b179b149d
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-06-09 10:23:14 -0300

    feat: .

---
+++ src/testing/Routines.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class Routines {
+    public static void main(String[] args) {
+        System.out.println(returnHelloWorld());
+    }
+
+    static String returnHelloWorld() {
+        return "Hello World";
+    }
+}
