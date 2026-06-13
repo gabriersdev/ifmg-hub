2b80ef523f54d67607eaaa77a85989a14e4cda86
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-06-13 12:45:17 -0300

    feat: update scripts

--- src/exercise09/Gabriel01.java
+++ src/exercise09/Gabriel01.java
@@ -1,5 +1,7 @@
 package exercise09;
 
+import java.util.Scanner;
+
 public class Gabriel01 {
     public static void main(String[] args) {
         // Questão 1 - Sistema de Cadastro de Usuário
@@ -27,5 +29,61 @@ public class Gabriel01 {
         // - Verifique se a senha possui pelo menos 8 caracteres.
         // - Verifique se possui pelo menos um número. -Verifique se a senha possui pelo menos um caractere especial.
         // - Exiba 'Senha forte', se todas as condições forem satisfeitas; ‘Senha média’, se pelo menos 2 das condições forem satisfeitas; ou 'Senha fraca' caso contrário.
+
+        String name = "", email = "", password = "";
+        String[] infos = {"nome", "e-mail", "senha"};
+        Boolean[] conditions;
+        int numberConditionsMet = 0;
+        Scanner input = new Scanner(System.in);
+
+        for (String info : infos) {
+            System.out.printf("Informe o seu(a) %s: ", info);
+            switch (info) {
+                case "nome" -> name = input.nextLine().trim();
+                case "e-mail" -> email = input.nextLine().trim();
+                case "senha" -> password = input.nextLine().trim();
+                default -> System.out.println("Há um erro!");
+            }
+        }
+
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty())
+            System.out.println("Você precisa preencher TODAS as informações solicitadas. Tente novamente.");
+
+            // Verifica as informações e retorna os dados
+        else {
+            // Informações do nome
+            System.out.printf(
+                    "%nO seu nome:%nEm letras minúsculas: \"%s\"%nEm letras maiúsculas: \"%s\"%nA quantidade de caracteres que ele possui: %d%n%n",
+                    name.toLowerCase(),
+                    name.toUpperCase(),
+                    name.length()
+            );
+
+            // Informações do e-mail
+            if (email.contains("@")) System.out.println("O e-mail informado é válido");
+            else System.out.println("O e-mail informado é inválido");
+
+            // Login
+            System.out.printf(
+                    "%nSeu login: \"%s\".%n%n",
+                    name.replaceAll("\\s", ".").replaceAll("\\.{2,}", ".").toLowerCase()
+            );
+
+            conditions = new Boolean[]{
+                    password.length() >= 8,
+                    // Procura um dígito em qualquer lugar na string password
+                    password.matches(".*\\d.*"),
+                    // Dá match apenas em caracteres especiais
+                    password.matches(".*\\p{Punct}.*")
+            };
+
+            // Verifica quantas condições foram atendidas (ou sejam, são true)
+            for (Boolean condition : conditions) if (condition) numberConditionsMet++;
+
+            // Senha
+            if (numberConditionsMet == 3) System.out.println("Senha forte.");
+            else if (numberConditionsMet == 2) System.out.println("Senha média.");
+            else System.out.println("Senha fraca.");
+        }
     }
 }
---
+++ src/testing/Regex.java
@@ -0,0 +1,7 @@
+package testing;
+
+public class Regex {
+    public static void main(String[] args) {
+        System.out.println("12345@0.a".matches(".*\\d.*"));
+    }
+}
