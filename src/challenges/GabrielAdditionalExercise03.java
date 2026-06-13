88fe38d70d7eaf0c54163653bd5ca4669d5572cc
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-06-13 12:55:12 -0300

    fix: correcao de GabrielAdditionalExercise03.java

--- src/challenges/GabrielAdditionalExercise03.java
+++ src/challenges/GabrielAdditionalExercise03.java
@@ -10,50 +10,44 @@ public class GabrielAdditionalExercise03 {
          * garantindo que o número seja ímpar e maior ou igual a 3. A árvore deve ser construída usando
          * o caractere '#' e espaços em branco, de forma que o tronco e a copa fiquem centralizados.
          */
-        double height;
-        int middle, i, j, k, intermediateHeight;
-        // LÓGICA: a quantidade de linhas PODE ser PAR. O que não pode ser PAR é a quantidade de itens na linha, pois não vai ficar legal.
-        // OBJETIVO: criar uma árvore de acordo com o que é informado pelo usuário
-        Scanner keyboard = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int height, i, j, k, canopyHeight, trunkSpaces;
 
         while (true) {
             System.out.println("Informe a quantidade de linhas que a árvore terá: ");
 
-            if (keyboard.hasNextInt()) {
+            if (scanner.hasNextInt()) {
+                height = scanner.nextInt();
 
-                intermediateHeight = keyboard.nextInt();
-                height = intermediateHeight;
-                System.out.println("height: " + height);
-
-                if (height % 2 != 1) {
-                    System.out.println("Por favor, informe um número ÍMPAR");
+                if (height % 2 == 0) {
+                    System.out.println("Por favor, informe um número ÍMPAR.");
                     continue;
                 }
 
-                //
-                else if (height < 3) {
-                    System.out.println("Por favor, informe um número MAIOR ou IGUAL à 3");
+                if (height < 3) {
+                    System.out.println("Por favor, informe um número MAIOR ou IGUAL a 3.");
                     continue;
                 }
 
-                // TODO - BUG - a quantidade de linhas não está funcionando!
-                middle = ((int) Math.ceil(height / 2)) + (height > 3 ? 1 : 0);
+                canopyHeight = height - 1;
+                trunkSpaces = canopyHeight - 1;
 
-                for (i = 0; i <= middle; i++) {
-                    // Parte de J = 0 e para na METADE da quantidade de linhas - I
-                    for (j = 0; j < middle - i; j++) System.out.print(" ");
-                    // Parte de K = 0 e para no DOBRO de I + 1
+                for (i = 0; i < canopyHeight; i++) {
+                    for (j = 0; j < trunkSpaces - i; j++) System.out.print(" ");
                     for (k = 0; k < (2 * i + 1); k++) System.out.print("#");
-
-                    // Pula a linha
                     System.out.println();
                 }
 
-                break;
-            }
+                for (j = 0; j < trunkSpaces; j++) System.out.print(" ");
+                System.out.println("#");
 
-            //
-            else System.out.println("O número informado precisa ser INTEIRO");
+                break;
+            } else {
+                System.out.println("O número informado precisa ser INTEIRO.");
+                scanner.next(); // Limpa a entrada inválida do scanner para evitar loop infinito
+            }
         }
+
+        scanner.close();
     }
 }
